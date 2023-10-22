a14ada9fcdae6c77a1bfec8af7f260f77b57e69c
ijyothir <jyothir.aadith.x@gmail.com>
2023-10-22 20:22:28 +0400

    list.py

---
+++ list.py/list.py
@@ -0,0 +1,15 @@
+#Homogeneous List / python supports Heterogenous list other than C , Java
+
+names = ["Jyothir" , "Bathspa" , "Codelab 1"]
+print(names)
+
+#Heterogenous list
+student =["Jyothir" , "123" , "3.18"]
+print(student)
+
+list_1 = [1,2,3,4,5,6,7,8,9,10] 
+print(list_1 * 8)
+
+#Positive Indexing numbers =[5,6,7,8,9]
+numbers =[5,6,7,8,9]
+print
