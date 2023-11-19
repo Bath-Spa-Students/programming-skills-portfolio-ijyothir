f030571aea6a2e54e51408eaee2d7ff65676063f
ijyothir <jyothir.aadith.x@gmail.com>
2023-11-19 13:57:18 +0400

    Create ex2.py

---
+++ Chapter 3- Data Structures/Exercises/ex2.py
@@ -0,0 +1,10 @@
+names = ['josh', 'gabriel', 'ethan']
+
+msg f"Hello, (names[0].title()}"
+print(msg)
+
+msg f"Hello, (names[1].title()}" 10 
+print(msg)
+
+msg = f"Hello, (names[2].title()}"
+print(msg)
