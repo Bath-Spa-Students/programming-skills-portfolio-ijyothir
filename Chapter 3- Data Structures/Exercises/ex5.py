31d30654c2bc4145b6d4d449c03f9a21ce7734a3
ijyothir <jyothir.aadith.x@gmail.com>
2023-11-19 14:10:49 +0400

    Create ex5.py

---
+++ Chapter 3- Data Structures/Exercises/ex5.py
@@ -0,0 +1,26 @@
+guests ['ali', 'Joseph', 'shafeeq']
+
+name guests[0].title() 
+print(f^{name}, please come to dinner.")
+
+name guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name guests [2].title()
+print(f"{name}, please come to dinner.")
+
+name guests[1].title()
+print(f"\nSorry, (name) can't make it to dinner.")
+
+#joseph can't make it! Let's invite osaid instead.
+del (guests[1]) guests.insert(1,'osaid')
+
+#Print the invitations again.
+name guests[0].title()
+print(f"\n{name}, please come to dinner.")
+
+name guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name guests[2].title()
+print(f"{name}, please come to dinner."
